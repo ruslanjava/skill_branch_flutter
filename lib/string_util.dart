1d0081c5c3a27f1b7c9711752b5d9b97d75a7f99
ruslanjava <ruslan.java@gmail.com>
2020-04-25 23:09:28 +0300

    MyString2 "capitalize2"

--- lib/string_util.dart
+++ lib/string_util.dart
@@ -1,7 +1,7 @@
 extension MyString on String {
-  String capitalize2() => this[0].toUpperCase() + this.substring(1).toLowerCase();
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1).toLowerCase();
 }
 
 extension MyString2 on String {
-  String capitalize(String s) => s[0].toUpperCase() + s.substring(1).toLowerCase();
+  String capitalize2() => this[0].toUpperCase() + this.substring(1).toLowerCase();
 }
